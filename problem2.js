02443ec1f2b7d56ec1db585727fad8ed1c3fcfbb
bony075 <bony.075@gmail.com>
2022-07-31 18:24:33 +0600

    problem 2

---
+++ problem2.js
@@ -0,0 +1,8 @@
+var math = 75.25;
+var bio = 65;
+var chemistry = 80;
+var phy = 35.45;
+var ban = 99.50;
+var totalmark = math + bio + chemistry + phy + ban;
+var result = totalmark / 5;
+console.log(result);
